da7f174590aa02cbe954e4a7285d4cde2d475e96
briyidricaldi <89927240+briyidricaldi@users.noreply.github.com>
2021-11-16 23:44:16 -0500

    Add files via upload

---
+++ tarea1_S13.py
@@ -0,0 +1,13 @@
+for x in range(1,5):
+	nombre=input("Ingrese nombre N "+str(x))
+for y in range(1,5):
+	nota=int(input("Ingrese nota N "+str(y))
+
+	if nota >= 13:
+		print(nota,"APROBADO")
+
+		else:
+
+	 	nota < 13:
+			print(nombre,"DESAPROBADO")
+
